8cce384bf3c197008542ea97aea1fabea466a7f0
Carlos Gabriel <140734200+Inojoza28@users.noreply.github.com>
2024-05-17 00:01:54 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 const darkModeToggle = document.getElementById('darkModeToggle');
-const body = document.body;
+    const body = document.body;
 
-darkModeToggle.addEventListener('click', () => {
-  body.classList.toggle('dark-mode');
-});
+    // Verifica se o modo selecionado está armazenado em localStorage
+    const savedMode = localStorage.getItem('mode');
 
+    // Se o modo estiver armazenado, aplica-o
+    if (savedMode === 'dark-mode') {
+        body.classList.add('dark-mode'); // Adiciona a classe do modo salvo ao body
+        darkModeToggle.checked = true; // Marca o checkbox
+    }
+
+    darkModeToggle.addEventListener('click', () => {
+        body.classList.toggle('dark-mode');
+
+        // Armazena o novo modo em localStorage
+        const currentMode = body.classList.contains('dark-mode') ? 'dark-mode' : '';
+        localStorage.setItem('mode', currentMode);
+    });
+
+    
+// Botão de scroll top
 
 function scrollToTop() {
   window.scrollTo({
